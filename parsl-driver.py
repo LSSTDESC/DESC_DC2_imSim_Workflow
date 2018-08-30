7fe774f50c7148accbea879a926e938c0fb72995
Ben Clifford <benc@hawaga.org.uk>
2018-08-30 22:10:25 +0000

    add fake mode

--- parsl-driver.py
+++ parsl-driver.py
@@ -22,6 +22,14 @@ cfg_inst_cat = "~benc/desc/benc.instcat/000025/instCat/phosim_cat_32678.txt"
 # for load testing, run this many times:
 cfg_repeated_runs = 7
 
+# set to true to use fake short sleep instead of singularity
+cfg_fake = True
+
+
+@bash_app
+def run_imsim_in_singularity_fake(work_out_dir_n: int, nthreads: int, pseudo_catalog: int, work_and_out_base: str, singularity_img_path: str, inst_cat: str):
+    return "sleep 20s"
+
 @bash_app
 def run_imsim_in_singularity(work_out_dir_n: int, nthreads: int, pseudo_catalog: int, work_and_out_base: str, singularity_img_path: str, inst_cat: str):
     pathbase = "{}/run-{}-{}/".format(work_and_out_base, work_out_dir_n, pseudo_catalog)
@@ -29,7 +37,6 @@ def run_imsim_in_singularity(work_out_dir_n: int, nthreads: int, pseudo_catalog:
     workdir = pathbase + "work/"
     return "singularity run {} --instcat {} --workdir {} --outdir {} --low_fidelity --subset_size 300 --subset_index 0 --file_id ckpt --processes {}".format(singularity_img_path, inst_cat, outdir, workdir, nthreads)
 
-    # return "singularity run /home/benc/desc2.0i/ALCF_1.2i/benc-cached.simg ~/desc/benc.instcat/000025/instCat/phosim_cat_32678.txt ~/desc2.0i/runtime/workdir-{}/ ~/desc2.0i/runtime/outdir-{}/ {} 0 10000 0 checkpoint_".format(work_out_dir_n, work_out_dir_n, nthreads)
 
 print("launching runs")
 
@@ -37,7 +44,12 @@ res = []
 
 for pseudo_catalog in range(0,cfg_repeated_runs):
   print("launching a run")
-  future = run_imsim_in_singularity(18, 63, pseudo_catalog, cfg_work_and_out_path, cfg_singularity_img, cfg_inst_cat)
+  if cfg_fake:
+      p = run_imsim_in_singularity_fake
+  else:
+      p = run_imsim_in_singularity
+    
+  future = p(18, 63, pseudo_catalog, cfg_work_and_out_path, cfg_singularity_img, cfg_inst_cat)
   print("launched a run")
   res.append(future)
 
