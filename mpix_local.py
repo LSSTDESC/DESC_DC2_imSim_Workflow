0de6d81ba72a5c0a222dada353e41c6f5977a82f
Ben Clifford <benc@hawaga.org.uk>
2018-08-30 17:45:51 +0000

    WIP - runs on a bunch of nodes

--- mpix_local.py
+++ mpix_local.py
@@ -4,11 +4,25 @@ from libsubmit.launchers import SimpleLauncher
 from parsl.config import Config
 from parsl.executors.mpix import MPIExecutor
 
+THETA_NODES=8
+ # this should scale up to 802 in real life, or up to 8 or 16 in debug queue
+ # note at present, 1 is required to run the rank 0 controller, not any app tasks
+ # so the minimum value to make progress is 2
+
+THETA_QUEUE="debug-flat-quad"
+WALLTIME="00:58:00"
+ACCOUNT="LSSTADSP_DESC"
+
+# ip (or hostname, probably) of the submitting host
+SUBMIT_HOST_IP = "10.236.1.193"
+
+# parameters above are ones you're likely to want
+# to change
+# stuff below, probably not so much
 
 
-IP = "10.236.1.193"
-JOB_URL = "tcp://{}:50005".format(IP)
-RESULT_URL = "tcp://{}:50006".format(IP)
+JOB_URL = "tcp://{}:50005".format(SUBMIT_HOST_IP)
+RESULT_URL = "tcp://{}:50006".format(SUBMIT_HOST_IP)
 
 launch_cmd='which python3; \
 aprun -b -cc depth -j 1 -d 64 -n $(($COBALT_PARTSIZE * {tasks_per_node})) -N {tasks_per_node} \
@@ -28,22 +42,20 @@ export OMP_NUM_THREADS=1"""
 config = Config(
     executors=[
         MPIExecutor(
-            label="parsl-mpi-executor",
+            label="parsl-mpix",
             jobs_q_url=JOB_URL,
             results_q_url=RESULT_URL,
             launch_cmd=launch_cmd,
             provider=CobaltProvider(
-                queue="debug-flat-quad",
-                # queue="default",                                                                                                                                                                                                             
+                queue=THETA_QUEUE,
                 launcher=SimpleLauncher(),
-                walltime="00:58:00",
-                nodes_per_block=2, # this should scale up to 802 in real life, or up to 8 or 16 in debug queue
-                                   # note at present, 1 is required to run the rank 0 controller, not any app tasks
+                walltime=WALLTIME,
+                nodes_per_block=THETA_NODES,
                 tasks_per_node=1,
                 init_blocks=1,
                 max_blocks=1,
                 overrides=overrides,
-                account="LSSTADSP_DESC",
+                account=ACCOUNT,
                 cmd_timeout=60
             ),
         )
--- parsl-driver.py
+++ parsl-driver.py
@@ -16,14 +16,28 @@ parsl.load(mpix_local.config)
 # of the mpix_local config
 
 @bash_app
-def run_imsim_in_singularity(work_out_dir_n: int, nthreads: int):
-    return "singularity run ~/desc2.0i/ALCF_1.2i/benclifford-ALCF.simg --instcat ~/desc/benc.instcat/000025/instCat/phosim_cat_32678.txt --workdir ~/desc/tmp-workdir{}/ --outdir ~/desc/tmp-outdir{}/ --low_fidelity --subset_size 300 --subset_index 0 --file_id CHKPT --processes {}".format(work_out_dir_n, work_out_dir_n, nthreads)
+def run_imsim_in_singularity(work_out_dir_n: int, nthreads: int, pseudo_catalog: int):
+    pathbase = "~/desc/run-{}-{}/".format(work_out_dir_n, pseudo_catalog)
+    outdir = pathbase + "out/"
+    workdir = pathbase + "work/"
+    return "singularity run ~/desc2.0i/ALCF_1.2i/benclifford-ALCF.simg --instcat ~/desc/benc.instcat/000025/instCat/phosim_cat_32678.txt --workdir {} --outdir {} --low_fidelity --subset_size 300 --subset_index 0 --file_id ckpt --processes {}".format(outdir, workdir, nthreads)
 
     # return "singularity run /home/benc/desc2.0i/ALCF_1.2i/benc-cached.simg ~/desc/benc.instcat/000025/instCat/phosim_cat_32678.txt ~/desc2.0i/runtime/workdir-{}/ ~/desc2.0i/runtime/outdir-{}/ {} 0 10000 0 checkpoint_".format(work_out_dir_n, work_out_dir_n, nthreads)
 
-print("launching urn")
-future = run_imsim_in_singularity(17, 63)
-print("launched run ... waiting for result")
-future.result()
+print("launching runs")
+
+res = []
+
+for pseudo_catalog in range(0,7):
+  print("launching a run")
+  future = run_imsim_in_singularity(18, 63, pseudo_catalog)
+  print("launched a run")
+  res.append(future)
+
+
+for f in res:
+  print("waiting for a future")
+  f.result()
+
 print("result received. the end")
 
