11a99fa66746dc5a77e3c6bdca86585f70586f9d
Ben Clifford <benc@hawaga.org.uk>
2018-09-09 12:10:27 +0000

    port print statements to use logging

--- parsl-driver.py
+++ parsl-driver.py
@@ -6,7 +6,15 @@ import pathlib
 import parsl
 from parsl.app.app import bash_app
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 parsl.set_stream_logger()
+parsl.set_stream_logger(__name__)
+
+logger.info("No-op log message to test log configuration")
 
 # import configuration after setting parsl logging, because interesting
 # construction happens during the configuration
@@ -68,7 +76,7 @@ def run_imsim_in_singularity(nthreads: int, work_and_out_base: str, singularity_
 def trickle_submit(task_info):
 
   instance_catalog = task_info
-  print("launching a run for instance catalog {}".format(instance_catalog))
+  logger.info("launching a run for instance catalog {}".format(instance_catalog))
 
   # TODO: factor this base 
   stuff_a = instance_catalog.replace(cfg_inst_cat_root, "ICROOT", 1)
@@ -79,21 +87,21 @@ def trickle_submit(task_info):
 
   pathlib.Path(workdir).mkdir(parents=True, exist_ok=True) 
 
-  print("app stdout/stderr logging: will create path {}".format(workdir))
+  logger.info("app stdout/stderr logging: will create path {}".format(workdir))
   ot = workdir + "task-stdout.txt"
   er = workdir + "task-stderr.txt"
  
   future = run_imsim(63, cfg_work_and_out_path, cfg_singularity_img, instance_catalog, cfg_inst_cat_root, stdout=ot, stderr=er)
-  print("launched a run for instance catalog {}".format(instance_catalog))
+  logger.info("launched a run for instance catalog {}".format(instance_catalog))
   return future
 
 
-print("listing instance catalogs")
+logger.info("listing instance catalogs")
 # This glob came from Jim Chiang
 instance_catalogs = glob.glob('{}/DC2-R1*/0*/instCat/phosim_cat*.txt'.format(cfg_inst_cat_root))
-print("there are {} instance catalogs to process".format(len(instance_catalogs)))
+logger.info("there are {} instance catalogs to process".format(len(instance_catalogs)))
 
-print("caching singularity image")
+logger.info("caching singularity image")
 
 
 if (not cfg_fake) and cfg_singularity_download:
@@ -106,7 +114,7 @@ if cfg_fake:
 else:
     run_imsim = run_imsim_in_singularity
 
-print("Starting up trickle-in loop")
+logger.info("Starting up trickle-in loop")
 
 
 todo_tasks = instance_catalogs
@@ -115,30 +123,30 @@ last_rebalance = time.time()
 
 while len(todo_tasks) > 0 or len(submitted_futures) > 0:
   balance_ago = time.time() - last_rebalance
-  print("trickle loop: looping - {} tasks still to submit, {} futures to wait for, last rebalance was {} ago".format(len(todo_tasks), len(submitted_futures), round(balance_ago) ))
+  logger.info("trickle loop: looping - {} tasks still to submit, {} futures to wait for, last rebalance was {} ago".format(len(todo_tasks), len(submitted_futures), round(balance_ago) ))
 
   if balance_ago > cfg_rebalance_seconds:
-    print("CALLBACK: rebalance")
+    logger.info("CALLBACK: rebalance")
     last_rebalance = time.time() # this will be roughly the time the rebalance finished, not cfg_rebalance_seconds after the last rebalance
 
   # check if any futures are finished, without blocking
   for f in submitted_futures:
     if f.done():
-      print("Future f is done")
-      print("CALLBACK: task done")
+      logger.info("Future f is done")
+      logger.info("CALLBACK: task done")
       submitted_futures.remove(f)
 
   while len(submitted_futures) < cfg_max_simultaneous_submit and len(todo_tasks) > 0:
-    print("There is capacity for a new task")
+    logger.info("There is capacity for a new task")
     task_info = todo_tasks.pop()
     f = trickle_submit(task_info)
     submitted_futures.append(f)
 
-  print("trickle loop: end iteration")
+  logger.info("trickle loop: end iteration")
   time.sleep(cfg_trickle_loop_seconds)
 
 
 
 
-print("end of parsl-driver")
+logger.info("end of parsl-driver")
 
