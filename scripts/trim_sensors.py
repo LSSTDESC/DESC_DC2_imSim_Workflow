76d6ad190574e986925dc2d51be515f4a0f0a91f
Jim Chiang <jchiang@slac.stanford.edu>
2018-09-19 12:15:28 -0700

    check if simulation region corner is likely inside a sensor region

--- scripts/trim_sensors.py
+++ scripts/trim_sensors.py
@@ -1,3 +1,8 @@
+"""
+Module to find the sensors that overlap the DC2 simulation region for a
+given visit.
+"""
+
 import warnings
 import numpy as np
 from lsst.afw import cameraGeom
@@ -7,6 +12,10 @@ with warnings.catch_warnings():
 import desc.imsim
 
 class Run20Region:
+    """
+    Class to find sensors in a given visit that intersect the
+    Run2.0 simulation region.
+    """
     def __init__(self, ra_mid=61.855, ne_corner=(71.46, -27.25),
                  dec_range=(-44.33, -27.25)):
         self._ra_mid = ra_mid
@@ -15,13 +24,33 @@ class Run20Region:
         self._dra_scale = np.abs(ra0 - self._ra_mid)*cos_dec0
         self._dec_range = dec_range
 
+        self.region_corners = []
+        for dec in dec_range:
+            dra = self._dra(dec)
+            self.region_corners.extend([(ra_mid - dra, dec),
+                                        (ra_mid + dra, dec)])
+
+    def _dra(self, dec):
+        return np.abs(self._dra_scale/np.cos(np.radians(dec)))
+
     def contains(self, ra, dec):
         if dec < min(self._dec_range) or dec > max(self._dec_range):
             return False
-        dra = np.abs(self._dra_scale/np.cos(np.radians(dec)))
-        if np.abs(ra - self._ra_mid) > dra:
+        if np.abs(ra - self._ra_mid) > self._dra(dec):
             return False
         return True
+
+    def contains_region_corners(self, sensor_corners):
+        ra_vals, dec_vals = [], []
+        for corner in sensor_corners:
+            ra_vals.append(corner[0])
+            dec_vals.append(corner[1])
+        ra_min, ra_max = min(ra_vals), max(ra_vals)
+        dec_min, dec_max = min(dec_vals), max(dec_vals)
+        for ra, dec in self.region_corners:
+            if all([ra > ra_min, ra < ra_min, dec > dec_min, dec < dec_max]):
+                return True
+        return False
 
     def trim_sensors(self, instcat):
         obs_md, _, _ \
