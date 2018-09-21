d8e523efcf0fec2193fb79bd3282ea147b6d7b0d
Ben Clifford <benc@hawaga.org.uk>
2018-09-21 15:45:28 +0000

    Remove dead code

--- scripts/parsl-bundle.py
+++ scripts/parsl-bundle.py
@@ -56,32 +56,3 @@ print("parsl-initial-bundle: Done")
 
 sys.exit(0)
 
-# We want to be able to check if a job has outputs. Since we haven't started the job,
-# this should fail all those tests. But we can test this with our utilities!
-
-infile = '/mnt/scripts/bundle_worklist_a.json' # the path to our file to check
-outpath = '/mnt/outputs/'                       # the path to out imSim outputs
-restartpath = '/mnt/restarts/'                  # where we are putting all our restarts
-
-# This checks for the existence of a gzipped file WHILE not having a checkpoint to measure
-# success, for each sensor that has work to do. Otherwise, it returns that information into
-# a new json file for restarting off of, in the requested restart path.
-jbu.check_job_success(infile, outpath, restartpath)
-
-# Let's make a new set of work to also do real quick.
-instcat_list_b = ['/mnt/cwd/DC2-R1-2p-WFD-g/000003/instCat/phosim_cat_159492.txt',
-                 '/mnt/cwd/DC2-R1-2p-WFD-g/000004/instCat/phosim_cat_159493.txt']
-ict.determine_instcat_work(instcat_list_b, '/mnt/scripts/instcat_worklist_b.json')
-with open('/mnt/scripts/instcat_worklist_b.json') as fp:
-    worklist_b = json.load(fp)
-bundle_list_b = jbu.determine_bundles(worklist_b, max_threads_node, max_instances_node)
-with open('/mnt/scripts/bundle_worklist_b.json', 'w') as fp:
-    json.dump(bundle_list_b, fp)
-
-# And now let's combine THIS with our restart data to generate a new master bundling.
-worklist_new = jbu.determine_remaining_jobs('/mnt/scripts/bundle_worklist_b.json', restartpath)
-bundle_list_new = jbu.determine_bundles(worklist_new, max_threads_node, max_instances_node)
-
-# And we can just save this new work list!
-with open('/mnt/scripts/bundle_worklist_new.json', 'w') as fp:
-    json.dump(bundle_list_new, fp)
