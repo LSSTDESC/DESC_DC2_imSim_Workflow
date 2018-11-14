786565344724b15697696816adb446e4b0ce9839
Antonio Villarreal <avillarreal@anl.gov>
2018-11-14 15:10:48 -0600

    fixed internal file paths

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/DC2
+cd /home/lsst/DC2
 source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_sims
 setup -r sims_photUtils -j
@@ -9,4 +9,4 @@ setup -r sims_GalSimInterface -j
 setup -r imSim -j
 setup -r obs_lsstCam -j
 
-python ~/DC2/ALCF_1.2i/scripts/run_imsim.py "$@"
+python /home/lsst/DC2/ALCF_1.2i/scripts/run_imsim.py "$@"
