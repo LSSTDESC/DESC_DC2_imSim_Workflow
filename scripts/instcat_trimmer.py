5cd0ce25a45dee0c15cbb4646758a0412b14f504
Antonio Villarreal <avillarreal@anl.gov>
2019-03-25 15:58:25 -0500

    added to instcat_trimmer to construct the initial database

--- scripts/instcat_trimmer.py
+++ scripts/instcat_trimmer.py
@@ -2,6 +2,7 @@ import json
 import os.path
 import sys
 import trim_sensors
+from progressdb import ProgressDB
 
 def determine_instcat_work(instcat_list, outfile):
     """Determine how many sensors each visit will take and the number of
@@ -12,12 +13,18 @@ def determine_instcat_work(instcat_list, outfile):
           instcat_list (list): a list of paths to all instance catalogs.
           outfile (str): path and name of where you want the master list to be written.
     """
-
+    db = ProgressDB() # connect to the database in order to start storing information about work.
     run20_region = trim_sensors.Run20Region()
     instcat_list = [os.path.abspath(instcat_file) for instcat_file in instcat_list]
     sensors_list = [run20_region.trim_sensors(instcat) for instcat in instcat_list]
     work_data = [ [instcat, sensors] for (instcat, sensors) in zip(instcat_list, sensors_list)]
     for work in work_data:
+        # first we want to add the instance catalog to the database.
+        visitname = os.path.basename(os.path.normpath(work[0]))
+        db.put_visit(visitname)
+        # and then for each sensor, we want to add it to the database.
+        for sensor in work[1]:
+            db.init_sensor_visit(visitname, sensor, False)
         numobj = [0 for sensor in work[1] ]
         work.append(numobj)
 
