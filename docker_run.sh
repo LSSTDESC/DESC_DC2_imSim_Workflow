6b2a711fbb8b58b19d34bb19ec7694f6d8d9a2b0
Antonio Villarreal <avillarreal@anl.gov>
2019-09-24 13:21:11 +0000

    changed docker_run.sh for new Dockerfile build

--- docker_run.sh
+++ docker_run.sh
@@ -2,7 +2,9 @@
 
 cd /DC2
 source /opt/lsst/software/stack/loadLSST.bash
-setup lsst_sims
+setup lsst_sims -t sims_w_2019_37
+setup -t DC2production throughputs
+setup -t DC2production sims_skybrightness_data
 setup -r imSim -j
 setup -r sims_GalSimInterface -j
 export OMP_NUM_THREADS=1
