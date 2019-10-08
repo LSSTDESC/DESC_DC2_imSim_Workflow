9976877dd9b03ba74f5ecdfad82da49ce55b5246
descim Pseudo User <descim@cori01.nersc.gov>
2019-10-08 11:09:17 -0700

    deactivate database now that parsl monitoring works

--- scripts/instcat_trimmer.py
+++ scripts/instcat_trimmer.py
@@ -13,7 +13,7 @@ def determine_instcat_work(instcat_list, outfile):
           instcat_list (list): a list of paths to all instance catalogs.
           outfile (str): path and name of where you want the master list to be written.
     """
-    db = ProgressDB() # connect to the database in order to start storing information about work.
+    #db = ProgressDB() # connect to the database in order to start storing information about work.
     run20_region = trim_sensors.Run20Region()
     #instcat_list = [os.path.abspath(instcat_file) for instcat_file in instcat_list]
     sensors_list = [run20_region.trim_sensors(instcat) for instcat in instcat_list]
@@ -21,11 +21,11 @@ def determine_instcat_work(instcat_list, outfile):
     for work in work_data:
         # first we want to add the instance catalog number.
         visit_id = int(os.path.basename(os.path.normpath(work[0])).split('_')[-1].split('.txt')[0])
-        db.put_visit(visit_id)
+        #db.put_visit(visit_id)
         # and then for each sensor, we want to add it to the database.
         for sensor in work[1]:
             sensor_name = ''.join([s for s in sensor if s.isdigit()])
-            db.init_sensor_visit(visit_id, sensor_name, False)
+            #db.init_sensor_visit(visit_id, sensor_name, False)
         numobj = [0 for sensor in work[1] ]
         work.append(numobj)
 
