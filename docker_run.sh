4a15cd35ac8805115ebfdb5d271c5f4ec18b0070
Heather Kelly <heather@slac.stanford.edu>
2019-10-23 09:44:14 -0400

    Updating docker-run.sh for Run2.2i
    Moving to lsst_sims w_2019_42 and removing sims_GalSimInterface as the default version available with the weekly is up to date.

--- docker_run.sh
+++ docker_run.sh
@@ -2,11 +2,10 @@
 
 cd /DC2
 source /opt/lsst/software/stack/loadLSST.bash
-setup lsst_sims -t sims_w_2019_37
+setup lsst_sims -t sims_w_2019_42
 setup -t DC2production throughputs
 setup -t DC2production sims_skybrightness_data
 setup -r imSim -j
-setup -r sims_GalSimInterface -j
 export OMP_NUM_THREADS=1
 
 echo docker_run.sh: will run command: "$@" >&2
